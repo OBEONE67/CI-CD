86bcbd0dc5095bdf8c6c0c7467ad840df3a8cc23
Smart Lights Dashboard <smartlights@dashboard.com>
2025-12-24 18:04:59 +0700

    fix: force tracking all files in backend folder

---
+++ my-ci-cd-backend/src/test/java/com/example/my_ci_cd_backend/MyCiCdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_ci_cd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCiCdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
