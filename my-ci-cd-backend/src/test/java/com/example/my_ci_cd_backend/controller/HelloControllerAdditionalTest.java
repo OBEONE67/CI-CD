3c646a401f6f9b217dfd4d7e2fcb3062daabf1c8
Smart Lights Dashboard <smartlights@dashboard.com>
2026-01-14 19:56:49 +0700

    test

--- my-ci-cd-backend/src/test/java/com/example/my_ci_cd_backend/controller/HelloControllerAdditionalTest.java
+++ my-ci-cd-backend/src/test/java/com/example/my_ci_cd_backend/controller/HelloControllerAdditionalTest.java
@@ -15,7 +15,7 @@ class HelloControllerAdditionalTest {
     @Test
     void echo_returnsSameString() {
         HelloController c = new HelloController();
-        assertEquals("ping", c.echo("pong"));
+        assertEquals("ping", c.echo("ping"));
     }
 
     @Test
